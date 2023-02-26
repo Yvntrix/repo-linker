68b0256860f81e5aeac2e5c8e4b55fcdba4c6e46
Ivanne Rencel Bano <ivannerencel28@gmail.com>
2023-02-26 16:17:37 +0800

    Fix issue not appearing when in repository

--- content.js
+++ content.js
@@ -6,10 +6,20 @@ const user = document.querySelector(".avatar-small");
 
 // Create a new link element for your repository
 const newLink = document.createElement("a");
-newLink.href = user.alt.slice(1) + "?tab=repositories";
+
 newLink.innerText = "Repositories";
-newLink.className =
-  "Header-link mt-md-n3 mb-md-n3 py-2 py-md-3 mr-0 mr-md-3 border-top border-md-top-0 border-white-fade";
+newLink.setAttribute(
+  "class",
+  "js-selected-navigation-item Header-link mt-md-n3 mb-md-n3 py-2 py-md-3 mr-0 mr-md-3 border-top border-md-top-0 border-white-fade"
+);
+newLink.setAttribute("data-hotkey", "g r");
+newLink.setAttribute(
+  "data-ga-click",
+  "Header, click, Nav menu - item:repositories context:user"
+);
+newLink.setAttribute("aria-label", "Your repositories");
+newLink.setAttribute("data-turbo", "false");
+newLink.setAttribute("href", `${user.alt.slice(1)}?tab=repositories`);
 
 // Add the new link element to the navigation bar
 navBar.insertBefore(newLink, navBar.firstChild);
